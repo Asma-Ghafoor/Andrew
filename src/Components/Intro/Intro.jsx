7e89d84d0542b4a2abf07a679026b7613e861d80
Asma-Ghafoor <asmaghafoor200@gmail.com>
2023-11-14 18:44:52 +0500

    React

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Intro.css'
+
+function Intro() {
+    return (
+        <div className="intro">
+            <div className="i-left">
+                <div className="i-name">
+                    <span>Hy! I Am <br /></span>
+                    <span>Asma Ghafoor <br /></span>
+                    <span>Frontend Developer with high
+                        level of experience in web designing
+                        and development,
+                        producting the Quality work</span>
+                </div>
+                <button className="button i-button">Hire me</button>
+            </div>
+            <div className="i-right">I am right side</div>
+
+        </div>
+    )
+}
+
+export default Intro
