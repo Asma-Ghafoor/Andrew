f91ed5581751fc928eddb899feb1904fadbfe871
Asma-Ghafoor <asmaghafoor200@gmail.com>
2023-11-15 15:03:53 +0500

    React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Intro from './Components/Intro/Intro';
-// import Github from '.Andrew/src/Components/img/github.png';
-// import Linkendin from './src/Components/img/Linkedin.png';
-import Github from '../../img/github.png'
+import Intro from './Components/Intro/Intro'
 
 function App() {
   return (
